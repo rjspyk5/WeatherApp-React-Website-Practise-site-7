be641033475350db041f427dc0e9f7a5968a6392
rjspyk5 <rjspyk5@gmail.com>
2023-12-05 18:53:09 +0600

    dynamic countryname and loading option added

--- src/Components/Body/TimeDateCountrySection/TimeDateCountry.jsx
+++ src/Components/Body/TimeDateCountrySection/TimeDateCountry.jsx
@@ -1,11 +1,26 @@
 import React, { useContext } from 'react';
 import { DataAndInputRcvrContext } from '../../../App';
+import { Loading } from '../Loading/Loading';
 
 export const TimeAndCountry = () => {
   const [, currentWeather, fivedayweatherr] = useContext(
     DataAndInputRcvrContext
   );
+
   const countryname = currentWeather.name;
+
+  const weatherType =
+    currentWeather &&
+    currentWeather.weather &&
+    currentWeather.weather.length > 0 ? (
+      <p className='text-[#34ebe1] text-center my-6'>
+        {currentWeather.weather[0].main}
+      </p>
+    ) : (
+      <Loading />
+    );
+
+  console.log(currentWeather);
   return (
     <>
       <div className='flex text-center text-white'>
@@ -14,9 +29,9 @@ export const TimeAndCountry = () => {
         </p>
       </div>
       <h3 className='text-center text-white font-bold my-6'>
-        {countryname ? countryname : 'London,GB'}
+        {countryname ? countryname : <Loading />}
       </h3>
-      <p className='text-[#34ebe1] text-center my-6'>Clouds</p>
+      {weatherType}
     </>
   );
 };
