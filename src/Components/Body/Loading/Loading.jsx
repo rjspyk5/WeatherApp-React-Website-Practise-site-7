bd30b88c1f761ea44c85024f461040ca8876fd68
rjspyk5 <rjspyk5@gmail.com>
2023-12-09 18:55:51 +0600

    Carousel and loading component install and added from internet

--- src/Components/Body/Loading/Loading.jsx
+++ src/Components/Body/Loading/Loading.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import SkeletonLoader from 'tiny-skeleton-loader-react';
 
 export const Loading = () => {
-  return <div>Loading..................</div>;
+  return (
+    <div>
+      <SkeletonLoader />
+    </div>
+  );
 };
